6b9a7b196a53c69602088d8023852159df2060d5
Genesis Liliana Alvarenga Zeron (24004) <genesis.alvarenga@est.zamorano.edu>
2022-09-12 19:05:07 -0600

    Lab 02 GAZ

---
+++ celcius2farenheit.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+
+temperaturaCelsius = float(input("Cual es la temperatura en Celsius? "))
+print(temperaturaCelsius)
+temperatureFahrenheit = (9/5) * temperaturaCelsius + 32
+print(temperatureFahrenheit)
